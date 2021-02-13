26bbd0d07bc3850fbe608a72e465bf123aee997a
Jymit <12527842+Jymit@users.noreply.github.com>
2021-02-13 16:53:44 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+ 
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+ 
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
